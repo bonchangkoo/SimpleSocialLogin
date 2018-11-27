7106584304fb021b4d32dc7e67bda226890bcf59
bonchang.koo <bonchang.koo@rgpkorea.co.kr>
2018-11-27 18:40:07 +0900

    modified Kakao AuthType

--- simplesociallogin/src/main/java/kr/co/yogiyo/simplesociallogin/kakao/KakaoSDKAdapter.kt
+++ simplesociallogin/src/main/java/kr/co/yogiyo/simplesociallogin/kakao/KakaoSDKAdapter.kt
@@ -8,7 +8,7 @@ class KakaoSDKAdapter(private val context: Context) : KakaoAdapter() {
     override fun getSessionConfig(): ISessionConfig {
         return object : ISessionConfig {
             override fun getAuthTypes(): Array<AuthType> {
-                return arrayOf(AuthType.KAKAO_LOGIN_ALL)
+                return arrayOf(AuthType.KAKAO_TALK)
             }
 
             override fun isUsingWebviewTimer(): Boolean {
